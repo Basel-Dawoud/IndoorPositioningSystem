2acf8d1b4467e87c335dde1283758e6e4f96a72b
Basel-Dawoud <134341247+Basel-Dawoud@users.noreply.github.com>
2026-04-25 23:14:04 +0300

    Delete server.py

--- server.py
+++
@@ -1,4 +0,0 @@
-# Getting starting with developing the server that will gather the updates from all the clients and save them in a data base then analyze them
-# The server will subsribe to all the clients on the mosquitto broker
-# We will develop the server using python asyncio
-
